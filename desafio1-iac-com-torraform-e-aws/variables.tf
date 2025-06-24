17e1bf1359156dc7bd771268a3a06afd1d04cf12
Mirtilo Almeida <mirtilo.almeida@rz-dmh5324.ad.razaoinfo.com.br>
2025-06-23 22:32:30 -0300

    fix: Corrected typo in 'terraform' directory name and consolidated files

--- desafio1-iac-com-torraform-e-aws/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "meu_ip_publico" {
-    type        = string
-    description = "Endereço IP público para o Security Group SSH"
-    # IMPORTANTE: Substitua pelo seu endereço IP público
-    # Para saber o seu IP público, acesse https://www.whatismyip.com/
-    default     = "203.0.113.25/32"
-}
